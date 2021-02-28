e88b9632ab1558f872e8896ac2e4bc93fc2c9d45
Rendy Artha <rendyartha312@gmail.com>
2021-02-28 20:17:21 +0700

    setup login register home

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
     <div>
-      Register
+      <h1>Form Register</h1>
+      <div>
+        <label>Nama</label>
+        <input type="text"/>
+      </div>
+      <div>
+        <label>Email</label>
+        <input type="email"/>
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password"/>
+      </div>
+      <div>
+        <button>Register</button>
+        <Link to="/">
+          Login Here
+        </Link>
+      </div>
     </div>
   )
 }
